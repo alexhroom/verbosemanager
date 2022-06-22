e1bdbc58c459e93b2a83b9b68abf3a76a4aaf371
alexhroom <alex.room@btinternet.com>
2022-06-22 11:28:40 +0100

    black tests

--- tests/test_decorator.py
+++ tests/test_decorator.py
@@ -6,44 +6,55 @@ from verbosemanager import verbosedecorator, VerboseManager
 
 
 # stdout for the processes at each verbosity level
-process_stdout = {1: "Process complete in 0.03 seconds.",
-                  2: ("Process complete in 0.03 seconds.\n"
-                      "Timings per step:\n"
-                      "Initialising: 0.01\n"
-                      "Process step 1: 0.01\n"
-                      "Process step 2: 0.01\n"),
-                  3: ("\n\r[                    ] 0%  Initialising "
-                      "\r[==========          ] 50%  Process step 1; previous step took 0.01 seconds. "
-                      "\r[====================] 100%  Process step 2; previous step took 0.01 seconds. \n"
-                      "Process complete in 0.03 seconds.\n"
-                      "Timings per step:\n"
-                      "Initialising: 0.01\n"
-                      "Process step 1: 0.01\n"
-                      "Process step 2: 0.01\n")}
+process_stdout = {
+    1: "Process complete in 0.03 seconds.",
+    2: (
+        "Process complete in 0.03 seconds.\n"
+        "Timings per step:\n"
+        "Initialising: 0.01\n"
+        "Process step 1: 0.01\n"
+        "Process step 2: 0.01\n"
+    ),
+    3: (
+        "\n\r[                    ] 0%  Initialising "
+        "\r[==========          ] 50%  Process step 1; previous step took 0.01 seconds. "
+        "\r[====================] 100%  Process step 2; previous step took 0.01 seconds. \n"
+        "Process complete in 0.03 seconds.\n"
+        "Timings per step:\n"
+        "Initialising: 0.01\n"
+        "Process step 1: 0.01\n"
+        "Process step 2: 0.01\n"
+    ),
+}
 
-bigger_process_stdout = {1: 'Bigger_Process complete in 0.05 seconds.\n',
-                         2: 'Bigger_Process complete in 0.05 seconds.\n'
-                            'Timings per step:\n'
-                            'Initialising: 0.01\n'
-                            'Bigger process step 1: 0.02\n'
-                            'Subprocess: \n'
-                            '|Process step 1: 0.01\n'
-                            '|Process step 2: 0.01\n',
-                         3: '\n\r[                    ] 0%  Initialising '
-                            '\r[======              ] 33%  Bigger process step 1; previous step took 0.01 seconds. '
-                            '\r[=============       ] 66%  Process step 1; previous step took 0.02 seconds.  '
-                            '\r[====================] 100%  Process step 2; previous step took 0.01 seconds.  \n'
-                            'Bigger_Process complete in 0.05 seconds.\n'
-                            'Timings per step:\n'
-                            'Initialising: 0.01\n'
-                            'Bigger process step 1: 0.02\n'
-                            'Subprocess: \n'
-                            '|Process step 1: 0.01\n'
-                            '|Process step 2: 0.01\n'}
+bigger_process_stdout = {
+    1: "Bigger_Process complete in 0.05 seconds.\n",
+    2: "Bigger_Process complete in 0.05 seconds.\n"
+    "Timings per step:\n"
+    "Initialising: 0.01\n"
+    "Bigger process step 1: 0.02\n"
+    "Subprocess: \n"
+    "|Process step 1: 0.01\n"
+    "|Process step 2: 0.01\n",
+    3: "\n\r[                    ] 0%  Initialising "
+    "\r[======              ] 33%  Bigger process step 1; previous step took 0.01 seconds. "
+    "\r[=============       ] 66%  Process step 1; previous step took 0.02 seconds.  "
+    "\r[====================] 100%  Process step 2; previous step took 0.01 seconds.  \n"
+    "Bigger_Process complete in 0.05 seconds.\n"
+    "Timings per step:\n"
+    "Initialising: 0.01\n"
+    "Bigger process step 1: 0.02\n"
+    "Subprocess: \n"
+    "|Process step 1: 0.01\n"
+    "|Process step 2: 0.01\n",
+}
 
 
 @parametrize("verbose", [1, 2, 3])
-@parametrize('func, n_steps, exp_stdout', [("process", 2, process_stdout), ("bigger_process", 3, bigger_process_stdout)])
+@parametrize(
+    "func, n_steps, exp_stdout",
+    [("process", 2, process_stdout), ("bigger_process", 3, bigger_process_stdout)],
+)
 def test_decorator(verbose, capsys, func, n_steps, exp_stdout):
     """Tests the verbosemanager decorator"""
 
@@ -63,12 +74,9 @@ def test_decorator(verbose, capsys, func, n_steps, exp_stdout):
         verbose_manager.step("Bigger process step 1")
         sleep(0.01)
         verbose_manager.header("Subprocess")
-        process(verbose=verbose['verbose'])
+        process(verbose=verbose["verbose"])
 
-    funcs = {
-        "process": process,
-        "bigger_process": bigger_process
-    }
+    funcs = {"process": process, "bigger_process": bigger_process}
 
     funcs[func](verbose=verbose)
     stdout = capsys.readouterr().out
